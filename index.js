cb678fb8800806374a0abd79562f8b3069e069d3
Milan Jakhaniya <milanpatel1082@gmail.com>
2023-09-06 01:00:00 +0530

    addded env variable for mongo url

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB (replace 'mongodb://localhost/yourdb' with your MongoDB connection string)
-const url = "mongodb+srv://milanjakhaniya:bdSsEEISS2ocDurU@cluster0.253vfsu.mongodb.net/demo?retryWrites=true&w=majority";
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const User = mongoose.model("user", {
   name: String,
